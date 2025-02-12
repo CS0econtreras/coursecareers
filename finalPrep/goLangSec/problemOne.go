bf0da71f4daa0b518a6c494ba47203733b5e79a9
CS0econtreras <ezeq317@gmail.com>
2025-02-11 22:03:33 -0700

    update

--- finalPrep/goLangSec/problemOne.go
+++ finalPrep/goLangSec/problemOne.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func twoLargest(numArr []int) []int {
+// better syntax
+func twoLargest(nums []int) []int {
+	largest := nums[0]
+	secondLargest := nums[1]
+
+	if secondLargest > largest {
+		largest, secondLargest = secondLargest, largest //swap without temp variable
+	}
+
+	for i := 2; i < len(nums); i++ {
+		if nums[i] > largest {
+			secondLargest = largest
+			largest = nums[i]
+		} else if nums[i] <= largest && nums[i] > secondLargest {
+			secondLargest = nums[i]
+		}
+	}
+	return []int{largest, secondLargest}
+}
+
+func main() {
+	sampleArr := []int{2, 15, 5, 7, 3, 6, 7, 32, 99, 1, 32}
+	fmt.Println(twoLargest(sampleArr))
+
+}
+
+//my version
+/*func twoLargest(numArr []int) []int {
 	largestNum := math.MinInt64
 	secondLargest := math.MinInt64
 
 	for i := 0; i < len(numArr); i++ {
 		if numArr[i] > largestNum {
+			secondLargest = largestNum
 			largestNum = numArr[i]
-		} else if numArr[i] < largestNum && numArr[i] > secondLargest {
+		} else if numArr[i] <= largestNum && numArr[i] > secondLargest {
 			secondLargest = numArr[i]
 		}
 	}
 	return []int{largestNum, secondLargest}
 }
-
-func main() {
-	sampleArr := []int{8, 1, 7, 14, 2, 10}
-	fmt.Println(twoLargest(sampleArr))
-	fmt.Println("yes")
-}
+*/
---
+++ finalPrep/goLangSec/problemTwo.go
@@ -0,0 +1,5 @@
+package main
+
+func highestOddSum(nums []int) []int {
+
+}
